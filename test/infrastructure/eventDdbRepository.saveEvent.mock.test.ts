49d926ceb27205f823c4ccf725778f2371cc84fb
Sakoda, Taro <sakoda.taro.229@gmail.com>
2025-01-27 09:05:15 +0900

    ✨ Update IEventRepository to include version parameter in getEventsByAggregateId; refactor saveEvent method and related tests for improved error handling

--- test/infrastructure/eventDdbRepository.saveEvent.mock.test.ts
+++ test/infrastructure/eventDdbRepository.saveEvent.mock.test.ts
@@ -1,16 +1,15 @@
 import {randomUUID} from 'node:crypto'
+import {ConditionalCheckFailedException} from '@aws-sdk/client-dynamodb'
 import {
   DynamoDBDocumentClient,
   PutCommand,
   QueryCommand,
 } from '@aws-sdk/lib-dynamodb'
 import {mockClient} from 'aws-sdk-client-mock'
-import {toHaveReceivedCommand} from 'aws-sdk-client-mock-vitest'
 import {
   DomainEvent,
   DomainEventName,
 } from '../../src/domain/entities/domainEvent'
-import {EventRepositoryErrorCode} from '../../src/domain/repositories/iEventRepository'
 import {EventDdbRepository} from '../../src/infrastructure/eventDdbRepository'
 
 const ddbMock = mockClient(DynamoDBDocumentClient)
@@ -46,27 +45,17 @@ describe('EventDdbRepository.saveEvent', () => {
       version: 1,
     })
     beforeEach(() => {
-      ddbMock.on(QueryCommand).resolves({
-        Items: [
-          {
-            PK: event.aggregateId,
-            SK: event.version,
-            created: event.createdAt,
-            name: event.name,
-            payload: event.payload,
-          },
-        ],
+      const error = new ConditionalCheckFailedException({
+        $metadata: {},
+        message: 'The conditional request failed',
       })
+      ddbMock.on(PutCommand).rejects(error)
     })
     it('throws an error, EventAlreadyExists', async () => {
       const repository = new EventDdbRepository()
       await expect(repository.saveEvent(event)).rejects.toThrow(
-        EventRepositoryErrorCode.EventAlreadyExists,
+        ConditionalCheckFailedException,
       )
-    })
-    it('does not save the event', async () => {
-      const repository = new EventDdbRepository()
-      expect(ddbMock).not.toHaveReceivedCommand(PutCommand)
     })
   })
   describe('Given an event which version is lower than the existing event', () => {
@@ -97,15 +86,9 @@ describe('EventDdbRepository.saveEvent', () => {
         ],
       })
     })
-    it('throws an error, EventAlreadyExists', async () => {
+    it('saves the event', async () => {
       const repository = new EventDdbRepository()
-      await expect(repository.saveEvent(event)).rejects.toThrow(
-        EventRepositoryErrorCode.EventAlreadyExists,
-      )
-    })
-    it('does not save the event', async () => {
-      const repository = new EventDdbRepository()
-      expect(ddbMock).not.toHaveReceivedCommand(PutCommand)
+      await expect(repository.saveEvent(event)).resolves.toBeUndefined()
     })
   })
 })
