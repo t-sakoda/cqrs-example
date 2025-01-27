e4fdd8d818e1b8a0a78649fc8f57b75583e4598c
Sakoda, Taro <sakoda.taro.229@gmail.com>
2025-01-27 09:06:33 +0900

    Add WidgetStockUpdated event to DomainEventName enumeration Introduce the WidgetStockUpdated event to the DomainEventName enumeration for enhanced event handling.

--- src/domain/entities/aggregate.ts
+++ src/domain/entities/aggregate.ts
@@ -1,3 +1,5 @@
+import type {DomainEvent} from './domainEvent'
+
 export const AggregateType = {
   Widget: 'Widget',
 } as const
@@ -5,24 +7,36 @@ export type AggregateType = (typeof AggregateType)[keyof typeof AggregateType]
 
 export interface AggregateProps {
   aggregateId: string
-  createdAt: string
-  type: AggregateType
-  lastEvents: any
-  version: number
 }
 
-export abstract class Aggregate {
+export class Aggregate {
   readonly aggregateId: string
-  readonly version: number
-  readonly createdAt: string
-  readonly lastEvents: any
-  readonly type: AggregateType
+  private _version?: number
+  private _createdAt?: string
+  private _lastEvents?: Record<number, DomainEvent>
+  private _type?: AggregateType
 
   constructor(props: AggregateProps) {
     this.aggregateId = props.aggregateId
-    this.createdAt = props.createdAt
-    this.type = props.type
-    this.lastEvents = props.lastEvents
-    this.version = props.version
+  }
+
+  get version() {
+    return this._version
+  }
+
+  get createdAt() {
+    return this._createdAt
+  }
+
+  get lastEvents() {
+    return this._lastEvents
+  }
+
+  get type() {
+    return this._type
+  }
+
+  applyEvent(event: DomainEvent) {
+    throw new Error('Method not implemented.')
   }
 }
